f03944840820928a1e1c0b5025c34298127617c4
Kirill Kopylov <0x80090002@gmail.com>
2021-01-15 16:17:42 +0200

    added task factory and seeder

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Task;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TaskFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Task::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'description' => $this->faker->text(500)
+        ];
+    }
+}
